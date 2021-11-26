e9fc74ca9ed76a313bc761f350c812b2698f9cb1
AdjoaJunior <estagyemang7804@gmail.com>
2021-11-26 15:51:29 +0000

    added grouped list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,13 @@
+
+
+import 'dart:html';
 import 'dart:js';
 
 import 'package:contact_application/contact_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'contact_model.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -30,21 +35,21 @@ class HomePage extends StatelessWidget {
        "region":"Bamako"
      },
      {
-      "name":"Austin Baby",
+      "name":"Osborn Gee",
        "phone":"+277 333 444 567",
        "email":"someone@example.com",
       "country":"Ethiopia",
       "region":"Addis Ababa"
      },
      {
-       "name":"Austin Baby",
+       "name":" Baby Mama",
        "phone":"+333 333 444 567",
        "email":"someone@example.com",
        "country":"Ethiopia",
        "region":"Addis Ababa"
      },
      {
-       "name":"Austin Baby",
+       "name":"Austina Dee",
        "phone":"+234 333 444 567",
        "email":"someone@example.com",
       "country":"Ghana",
@@ -111,7 +116,13 @@ class HomePage extends StatelessWidget {
                   onTap: (){
                     Navigator.of(context).push(
                       MaterialPageRoute(builder:(BuildContext context){
-                        return const ContactView();
+                        return  ContactView(contact: Contact(
+                          name: "Adjoa Agyemang",
+                          phone: '+233 247 18 7804',
+                          email: 'estagyemang@gmail.com',
+                          country: 'london',
+                          region: 'Cape Town'
+                        ),);
                       })
                       );
 
@@ -142,83 +153,47 @@ class HomePage extends StatelessWidget {
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                "A",
-                textAlign: TextAlign.right,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-              ),
-            ),
-            ListView.separated(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context){
-                        return const ContactView();
-                      }));
-                    },
-                    leading: const CircleAvatar(
-                      backgroundImage: AssetImage('Assets/828.jpg'),
-                    ),
-                    title:const Text(
-                      'Maa Adjoa',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-                    ),
-                    subtitle:const Text('+233 247 187 804'),
-                    trailing:const Icon(
-                      Icons.more_horiz,
-                      size: 28,
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 8,
-                  );
-                },
-                itemCount: 2
+            GroupedListView<Map<String,String>,String> (
+              shrinkWrap: true,
+              elements: data,
+             groupBy: (element) =>
+              element["name"].toString().substring(0,1),
+              groupSeparatorBuilder: (String groupByValue)=> SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(),
+                  child: Text(groupByValue,
+                  textAlign: TextAlign.right,
+                  style:const TextStyle(fontSize: 18,fontWeight: FontWeight.w600),
+                  ),
                 ),
-                 const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                "B",
-                textAlign: TextAlign.right,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
               ),
-            ),
-            ListView.separated(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return  ListTile(
-                    onTap:(){
-                      Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context){
-                        return const ContactView();
-                      }));
-                    }, 
+              itemBuilder: (context, Map<String,String> element){
+                Contact contact = Contact.fromJson(element);
+                return Column(children: [
+                  ListTile(
+                    onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                        return ContactView(contact:contact);
+                      }
+                      ),
+                      );
+                    },
                     leading:const CircleAvatar(
-                      backgroundImage: AssetImage('Assets/828.jpg'),
+                      backgroundImage:  AssetImage('assets/828.jpg'),
                     ),
-                    title:const Text(
-                      'Maa Adjoa',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-                    ),
-                    subtitle:const Text('+233 247 187 804'),
-                    trailing:const Icon(
-                      Icons.more_horiz,
-                      size: 28,
-                    ),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(
-                    height: 8,
-                  );
-                },
-                itemCount: 2)
+                    title: Text('${element["name"]}',
+                    style:const TextStyle(fontWeight: FontWeight.w600,fontSize: 20),),
+                    subtitle: Text('${element["phone"]}'),
+                  ),
+                  const Divider(),
+                ],
+                );
+              },
+               itemComparator:   (item1,item2)=>
+               item1['name']!.compareTo(item2['name']!),
+            ),
+
           ],
         ),
         ),
